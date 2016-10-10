6e3c56941df3a416e082a9f0d054b61a022291cb
Julian Krenge <julian@parcellab.com>
2016-10-10 13:16:40 +0200

    Buttonify mobile contact links
    Well, yeah.

--- src/hbs/generateContactLink.js
+++ src/hbs/generateContactLink.js
@@ -1,6 +1,8 @@
-module.exports = function generateContactLink(link) {
+module.exports = function generateContactLink(link, buttonify) {
+  var style = buttonify ? ' class="btn btn-default btn-block"' : '';
+
   if (/\S+@\S+\.\S+/.test(link))
-    return `<a href="mailto:${link}"><i class="fa fa-fw fa-envelope-o"></i> ${link}</a>`;
+    return `<a href="mailto:${link}" ${style}><i class="fa fa-fw fa-envelope-o"></i> ${link}</a>`;
   if (/^http.*/.test(link))
-    return `<a href="${link}" _target="blank"><i class="fa fa-fw fa-question-circle"></i> ${link}</a>`;
+    return `<a href="${link}" _target="blank" ${style}><i class="fa fa-fw fa-question-circle"></i> ${link}</a>`;
 };
--- src/hbs/generatePhoneLink.js
+++ src/hbs/generatePhoneLink.js
@@ -1,3 +1,4 @@
-module.exports = function generatePhoneLink(link) {
-  if (link) return `<a href="tel:${link}"><i class="fa fa-fw fa-phone"></i> ${link}</a>`;
+module.exports = function generatePhoneLink(link, buttonify) {
+  var style = buttonify ? ' class="btn btn-default btn-block"' : '';
+  if (link) return `<a href="tel:${link}" ${style}><i class="fa fa-fw fa-phone"></i> ${link}</a>`;
 };
