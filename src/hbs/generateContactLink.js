b30b477268eb878758fbefe0aeee3deac9c40b66
Andrej Fritz <fritz.andrej@me.com>
2016-12-12 15:52:15 +0100

    linke one-liner

--- src/hbs/generateContactLink.js
+++ src/hbs/generateContactLink.js
@@ -2,7 +2,7 @@ module.exports = function generateContactLink(link, buttonify) {
   var style = buttonify ? ' class="btn btn-default btn-block"' : '';
 
   if (/\S+@\S+\.\S+/.test(link))
-    return `<a href="mailto:${link}" ${style}><i class="fa fa-fw fa-envelope-o"></i> ${link}</a>`;
+    return `<a href="mailto:${link}" class="one-liner" ${style}><i class="fa fa-fw fa-envelope-o"></i> ${link}</a>`;
   if (/^http.*/.test(link))
-    return `<a href="${link}" _target="blank" ${style}><i class="fa fa-fw fa-question-circle"></i> ${link}</a>`;
+    return `<a href="${link}" class="one-liner" _target="blank" ${style}><i class="fa fa-fw fa-question-circle"></i> ${link}</a>`;
 };
