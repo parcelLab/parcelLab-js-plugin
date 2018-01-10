409adfccb2cd5f3072e3a38af8ec0a56e39cfea2
Andrej Fritz <fritz.andrej@me.com>
2018-01-10 14:43:44 +0100

    added order stats text

--- src/js/components/Header.js
+++ src/js/components/Header.js
@@ -2,11 +2,26 @@ const html = require('bel')
 const { translate } = require('../../js/lib/translator.js')
 const Tabs = require('./Tabs')
 
+const OrderStats = header => {
+  if (header) {
+    const orderStats = {}
+    header.filter(h => h.last_delivery_status).forEach(({ last_delivery_status }) => {
+      if (orderStats[last_delivery_status.status]) orderStats[last_delivery_status.status] + 1
+      else orderStats[last_delivery_status.status] = 1
+    })
+
+    if (Object.keys(orderStats).length > 0) {
+      return Object.keys(orderStats).map(stat => `${ orderStats[stat] } ${ stat }`).join(', ')
+    }
+  } else return null
+}
+
 const Header = (state, emit) => {
   const tabs = Tabs(state, emit)
   const { query } = state
   if (tabs && query.orderNo) {
     const { header } = state.checkpoints
+    const orderStatsText = OrderStats(header)
 
     return html`
     <div class="pl-header">
@@ -17,7 +32,10 @@ const Header = (state, emit) => {
 
           <div class="pl-order-sub">
             ${ translate('containsOf', query.lang.code)} ${header.length} ${translate('deliveries', query.lang.code) }
+
+            ${ orderStatsText ? `(${ orderStatsText })` : null }
           </div>
+
 
       </div>
       <div class="pl-col-row">${ tabs }</div>
