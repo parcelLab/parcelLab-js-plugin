fef6ab80182b98da9ddacf68c60308b83749cfba
Andrej Fritz <fritz.andrej@me.com>
2018-02-06 18:59:46 +0100

    small fix for caching

--- src/js/components/Icon.js
+++ src/js/components/Icon.js
@@ -4,7 +4,7 @@ const html = require('bel')
 // https://icongr.am/
 
 module.exports = function Icon(iconName, hexColor='333333', size='32') {
-  const path = `https://icongr.am/clarity/${iconName}.svg?color=${hexColor}&size=${size}`
+  const path = `https://icongr.am/clarity/${iconName}.svg?color=${hexColor}`
 
-  return html`<img src="${path}" class="pl-img-responsive" />`
+  return html`<img src="${path}" class="pl-img-responsive" style="max-width: ${size};" />`
 }
