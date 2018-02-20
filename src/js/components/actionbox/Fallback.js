71bb4dbd768b241721cf9e547f0aa7579a494df3
Andrej Fritz <fritz.andrej@me.com>
2018-02-20 18:28:40 +0100

    fallback iconsize

--- src/js/components/actionbox/Fallback.js
+++ src/js/components/actionbox/Fallback.js
@@ -3,11 +3,22 @@ const raw = require('bel/raw')
 const Icon = require('../Icon')
 const transitStates = require('../../lib/static').transitStates
 
+// hackish because of clarity icons ...
+const makeBig = el => {
+  if (el && el.style) {
+    el.style.width = '80px'
+    el.style.height = '80px'
+  }
+
+  return el
+}
+
 module.exports = function Fallback(tHeader) {
   if (!tHeader || !tHeader.last_delivery_status) return null
   const { code, status, status_details } = tHeader.last_delivery_status
   const transitState = transitStates[code] || transitStates.default
   const icon = transitState ? Icon(transitState.icon, null, '80') : null
+  makeBig(icon)
 
   return html`
     <div class="pl-box pl-action-box pl-box-icon-status">
