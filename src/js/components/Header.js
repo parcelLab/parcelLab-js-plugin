7ec1d5819257885645972ef0eba9577be0e66e67
Andrej Fritz <fritz.andrej@me.com>
2018-03-28 16:35:30 +0200

    always show orderNo if given

--- src/js/components/Header.js
+++ src/js/components/Header.js
@@ -22,8 +22,9 @@ const OrderStats = header => {
 const Header = (state, emit) => {
   const tabs = Tabs(state, emit)
   const { query } = state
+  const { header } = state.checkpoints
+
   if (tabs && query.orderNo) {
-    const { header } = state.checkpoints
     const orderStatsText = OrderStats(header)
 
     return html`
@@ -44,6 +45,20 @@ const Header = (state, emit) => {
       <div class="pl-col-row">${ tabs }</div>
     </div>
     `
+  } else if (!tabs && query.orderNo) {
+    return html`
+      <div class="pl-header">
+        <div class="pl-col-row">
+          <div class="pl-order-no">
+            ${translate('orderNo', query.lang.code)} ${query.orderNo}
+          </div>
+
+          <div class="pl-order-sub">
+            ${translate('containsOf', query.lang.code)} ${header.length} ${translate('deliveries', query.lang.code)}
+          </div>
+
+        </div>
+      </div>`
   } else return null
 }
 
