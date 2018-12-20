aece62bd08002daeee2ceef9bfbc91ea2245b16b
Georg Wild <georg@parcellab.com>
2018-12-20 17:21:08 +0100

    banner can haz rounded corners too!

--- src/js/components/Banner.js
+++ src/js/components/Banner.js
@@ -5,7 +5,7 @@ module.exports = function Banner({ options }) {
   return html`
   <div class="pl-box-aside-right pl-col pl-col-4">
     <a href="${ options.banner_link}" target="_blank">
-      <img class="pl-img-responsive" src="${ options.banner_image}" style="border:none;">
+      <img class="pl-img-responsive pl-box" src="${ options.banner_image}" style="border:none;">
     </a>
   </div>
   `
