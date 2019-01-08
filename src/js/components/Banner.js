d86b81cfba016d988439636f38b24e19ad2425d3
Andrej Fritz <fritz.andrej@me.com>
2019-01-08 12:28:10 +0100

    return null if no utl from insta

--- src/js/components/Banner.js
+++ src/js/components/Banner.js
@@ -3,6 +3,8 @@ const html = require('nanohtml')
 const isInstagram = url => (url.indexOf('instagram') >= 0)
 
 module.exports = function Banner({ options }) {
+  if (options.banner_link === 'instagram') return null
+
   return html`
   <div class="pl-box-aside-right pl-col pl-col-4">
     <a href="${ options.banner_link}" target="_blank">
