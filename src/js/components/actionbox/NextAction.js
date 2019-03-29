3a3956040400b64ff0d16c3943c3b7eb3dc6f45e
Andrej Fritz <fritz.andrej@me.com>
2019-03-29 14:29:37 +0100

    next action fix

--- src/js/components/actionbox/NextAction.js
+++ src/js/components/actionbox/NextAction.js
@@ -1,14 +1,18 @@
 const html = require('nanohtml')
 const Icon = require('../Icon')
 
-const NextAction = ({ actionBox }) => {
-  if (!actionBox.label) return null
+const NextAction = ({ actionBox, last_delivery_status }) => {
+  const { status, status_details } = last_delivery_status
+  if (!actionBox.label && !status) return null
+
+  let label = actionBox.label || status
+  let details = actionBox.caption || status_details
 
   return html`
     <div class="pl-box pl-action-box pl-box-icon-status">
 
       <div class="pl-box-heading">
-        ${ actionBox.label }        
+        ${ label }        
       </div>
 
       <div class="pl-box-body">
@@ -16,7 +20,7 @@ const NextAction = ({ actionBox }) => {
       </div>
 
       <div class="pl-box-footer">
-        <div>${ actionBox.caption }</div>
+        <div>${ details }</div>
       </div>
     </div>
   `
