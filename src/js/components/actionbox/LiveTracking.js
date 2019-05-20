318a7d2f01be63a6d1abbe755a231c13fa666b9a
Konstantin Baumer <konstantin@parcellab.com>
2019-05-20 11:20:40 +0200

    Display client specific truck icons

--- src/js/components/actionbox/LiveTracking.js
+++ src/js/components/actionbox/LiveTracking.js
@@ -4,16 +4,16 @@ const { translate } = require('../../../js/lib/translator.js');
 
 const generateMapSrc = address => `https://www.google.com/maps/embed/v1/place?key=${GOOGLE_API_KEY}&q=${encodeURIComponent(address)}&zoom=11`;
 
-const Map = (id, actionBox, courier) => {
+const Map = (id, actionBox, courier, query) => {
   const elem = html`
     <div id="pl-live-location-map" data-tid="${id}">
         <iframe id="pl-map-iframe" src="${generateMapSrc(
-            `${actionBox.info.cty},${actionBox.info.ctry.n}`
-            )}" frameborder="0" style="width:100%;height:100%;border:0px;z-index:2"">
+          `${actionBox.info.cty},${actionBox.info.ctry.n}`
+        )}" frameborder="0" style="width:100%;height:100%;border:0px;z-index:2"">
         </iframe>
         <a href="${courier.trackingurl}">
         <div id="pl-map-overlay">
-          <img id="pl-truck-icon" src="https://img.icons8.com/color/48/000000/in-transit.png" alt="" />
+          <img id="pl-truck-icon" src="http://cdn.parcellab.com/img/mail/_/truckonmap/${query.userId}.png" alt="" />
         </div>
       </a>
     </div>
@@ -27,7 +27,7 @@ const Map = (id, actionBox, courier) => {
   return elem;
 };
 
-const LiveTracking = ({ id, actionBox, last_delivery_status, courier }, lang, emit) => {
+const LiveTracking = ({ id, actionBox, last_delivery_status, courier }, query) => {
   if (!actionBox.info.cty) return null;
 
   const heading = last_delivery_status
@@ -43,11 +43,16 @@ const LiveTracking = ({ id, actionBox, last_delivery_status, courier }, lang, em
       ${heading}
 
       <div class="pl-box-body pl-box-location-body">
-        ${Map(id, actionBox, courier)}
+        ${Map(id, actionBox, courier, query)}
 
         <div class="pl-location-link-container">
-          <a href="${courier.trackingurl}" title="${translate('liveDelivery', lang.name)}" target="_blank" class="pl-button pl-is-fullwidth pl-location-link">
-            ${translate('liveDelivery', lang.name)}
+          <a
+            href="${courier.trackingurl}"
+            title="${translate('liveDelivery', query.lang.name)}"
+            target="_blank"
+            class="pl-button pl-is-fullwidth pl-location-link"
+          >
+            ${translate('liveDelivery', query.lang.name)}
           </a>
         </div>
       </div>
--- src/js/components/actionbox/index.js
+++ src/js/components/actionbox/index.js
@@ -50,8 +50,8 @@ const ActionBox = ({ checkpoints, activeTracking, query, options }, emit) => {
       return [NextAction(tHeader), ArticleList(tHeader, query.lang, options)]
     case 'returned':
       return Returned(tHeader)
-    case 'live-tracking':
-      return LiveTracking(tHeader, query.lang, emit)
+    case 'live-tracking':    
+      return LiveTracking(tHeader, query)
     default:
       return [
         Fallback(tHeader, options),
