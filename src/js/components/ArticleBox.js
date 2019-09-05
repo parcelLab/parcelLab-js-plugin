4e55599dd1dbc101195671303b923769cd919707
Andrej Fritz <fritz.andrej@me.com>
2019-09-05 13:33:12 +0200

    fix for articleAttrs

--- src/js/components/ArticleBox.js
+++ src/js/components/ArticleBox.js
@@ -12,14 +12,18 @@ function calcArticleTotalSum(validArticles) {
   return sum
 }
 
-function ArticleItem({ articleName, articleNo, quantity, imageUrl, productUrl }) {
+function ArticleItem({ articleName, articleNo, quantity, imageUrl, productUrl , articleImageUrl, articleUrl }) {
+  // because of js caching...
+  imageUrl = imageUrl || articleImageUrl || null
+  productUrl = productUrl || articleUrl || null
+
   let articleImage = null
 
   if (imageUrl) {
     if (productUrl)
-      articleImage = html`<a href="${productUrl}" target="_blank"><img class="pl-img-article-preview" src="${imageUrl}" alt="${articleName}"></a>`
+      articleImage = html`<a href="${productUrl}" target="_blank"><img class="pl-img-article-preview" src="${imageUrl}" alt="${articleName || articleNo}"></a>`
     else
-      articleImage = html`<img class="pl-img-article-preview" src="${imageUrl}" alt="${articleName}">`
+      articleImage = html`<img class="pl-img-article-preview" src="${imageUrl}" alt="${articleName || articleNo}">`
   } else {
     articleImage = Icon('no_camera', '000', '40')
     articleImage.style.margin = 'auto'
@@ -34,8 +38,9 @@ function ArticleItem({ articleName, articleNo, quantity, imageUrl, productUrl })
         </div>
         <div class="pl-col" style="width:70%;">
           <div class="pl-article-description">
-            ${ articleNo ? html`<span class="pl-article-list-no">${ articleNo }</span>` : '' } 
-            ${ quantity ? html`<span class="pl-article-quantity">${ quantity }x</span>` : '' }${ articleName }
+            ${ (articleNo && articleName) ? html`<span class="pl-article-list-no">${ articleNo }</span>` : '' } 
+            ${ quantity ? html`<span class="pl-article-quantity">${ quantity }x</span>` : '' }
+            ${ articleName || articleNo }
           </div>
       </div>
     </li>
@@ -51,7 +56,7 @@ module.exports = function ArticleList({ activeTracking, checkpoints, query, show
 
 
   if (activeTrackingArticles && activeTrackingArticles.length > 0) {
-    const validArticles = activeTrackingArticles.filter(a => a.articleName)
+    const validArticles = activeTrackingArticles.filter(a => (a.articleNo || a.articleName))
     if (validArticles.length < 1) return null
 
     let articleList = validArticles.map(ArticleItem)
