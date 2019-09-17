b8f03fc25ea266193b8472559f4a52e9c0941026
Andrej Fritz <fritz.andrej@me.com>
2019-09-17 15:03:01 +0200

    stage

--- src/js/components/ArticleBox.js
+++ src/js/components/ArticleBox.js
@@ -4,7 +4,7 @@ const T = require('../lib/translator.js')
 const MoreButton = require('./MoreButton')
 const { translate } = require('../lib/translator.js')
 
-function calcArticleTotalSum(validArticles) {
+function calcArticleTotalSum (validArticles) {
   let sum = 0
   validArticles.forEach(({ quantity = 1 }) => {
     sum = sum + parseInt(quantity)
@@ -12,7 +12,7 @@ function calcArticleTotalSum(validArticles) {
   return sum
 }
 
-function ArticleItem({ articleName, articleNo, quantity, imageUrl, productUrl , articleImageUrl, articleUrl }) {
+function ArticleItem ({ articleName, articleNo, quantity, imageUrl, productUrl, articleImageUrl, articleUrl }) {
   // because of js caching...
   imageUrl = imageUrl || articleImageUrl || null
   productUrl = productUrl || articleUrl || null
@@ -20,40 +20,40 @@ function ArticleItem({ articleName, articleNo, quantity, imageUrl, productUrl , 
   let articleImage = null
 
   if (imageUrl) {
-    if (productUrl)
+    if (productUrl) {
       articleImage = html`<a href="${productUrl}" target="_blank"><img class="pl-img-article-preview" src="${imageUrl}" alt="${articleName || articleNo}"></a>`
-    else
+    } else {
       articleImage = html`<img class="pl-img-article-preview" src="${imageUrl}" alt="${articleName || articleNo}">`
+    }
   } else {
     articleImage = Icon('no_camera', '000', '40')
     articleImage.style.margin = 'auto'
-    articleImage.style.opacity = '.2'  
+    articleImage.style.opacity = '.2'
   }
 
   return html`
     <li>
       <div class="pl-col-row">
         <div class="pl-col" style="text-align: center;width:30%;">
-          ${ articleImage }
+          ${articleImage}
         </div>
         <div class="pl-col" style="width:70%;">
           <div class="pl-article-description">
-            ${ (articleNo && articleName) ? html`<span class="pl-article-list-no">${ articleNo }</span>` : '' } 
-            ${ quantity ? html`<span class="pl-article-quantity">${ quantity }x</span>` : '' }
-            ${ articleName || articleNo }
+            ${(articleNo && articleName) ? html`<span class="pl-article-list-no">${articleNo}</span>` : ''} 
+            ${quantity ? html`<span class="pl-article-quantity">${quantity}x</span>` : ''}
+            ${articleName || articleNo}
           </div>
       </div>
     </li>
   `
 }
 
-module.exports = function ArticleList({ activeTracking, checkpoints, query, showAllArticles, articleList }, emit) {
+module.exports = function ArticleList ({ activeTracking, checkpoints, query, showAllArticles, articleList }, emit) {
   if (!articleList) return null
 
   const { lang } = query
   const { id } = checkpoints.header[activeTracking]
   const activeTrackingArticles = articleList[id]
-
 
   if (activeTrackingArticles && activeTrackingArticles.length > 0) {
     const validArticles = activeTrackingArticles.filter(a => (a.articleNo || a.articleName))
@@ -68,21 +68,19 @@ module.exports = function ArticleList({ activeTracking, checkpoints, query, show
     }
 
     return html`
-    <div class="pl-box-aside-right pl-col pl-col-4">
-      <div class="pl-box pl-box-articles ${ showAllArticles ? 'pl-scrollable' : ''}">
+      <div class="pl-box pl-box-articles ${showAllArticles ? 'pl-scrollable' : ''}">
         <div class="pl-box-heading">
-          ${ translate('articleList', lang.name) } (${calcArticleTotalSum(validArticles)})
+          ${translate('articleList', lang.name)} (${calcArticleTotalSum(validArticles)})
         </div>
         <div class="pl-box-body" style="padding-top:0;">
           <ul class="pl-article-list">
-            ${ articleList }
+            ${articleList}
           </ul>
         
-          ${ moreButton }
+          ${moreButton}
         </div>
 
       </div>
-    </div>
     `
   }
 
