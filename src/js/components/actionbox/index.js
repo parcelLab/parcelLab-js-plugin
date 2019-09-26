229dc2d3e38627552cc277d36e77af4297041315
Andrej Fritz <fritz.andrej@me.com>
2019-09-26 14:08:03 +0200

    better actionbox

--- src/js/components/actionbox/index.js
+++ src/js/components/actionbox/index.js
@@ -2,7 +2,6 @@ const PickupLocation = require('./PickupLocation')
 const Prediction = require('./Prediction')
 const VoteCourier = require('./VoteCourier')
 const PickupLocationUnknown = require('./PickupLocationUnknown')
-const OrderProcessed = require('./OrderProcessed')
 const NextAction = require('./NextAction')
 const Returned = require('./Returned')
 const Fallback = require('./Fallback')
@@ -11,45 +10,56 @@ const LiveTracking = require('./LiveTracking')
 
 const ActionBox = ({ checkpoints, activeTracking, query, options }, emit) => {
   const tHeader = checkpoints.header[activeTracking]
+
   if (tHeader && tHeader.actionBox) {
-    switch (tHeader.actionBox.type) {
-    case 'pickup-location':
-      if (tHeader.actionBox.data)
-        return PickupLocation(tHeader, query.lang, emit)
-      else
-        return Fallback(tHeader, options)
-    case 'vote-courier':
-      return [
-        Fallback(tHeader, options),
-        VoteCourier(tHeader, options, emit),
-      ]
-    case 'prediction':
-      if (tHeader.actionBox.data &&
-          (tHeader.actionBox.data.dayOfWeek || tHeader.actionBox.data.deliveryLocation))
-        return Prediction(tHeader)
-      else
-        break
-    case 'pickup-location-unknown':
-      return PickupLocationUnknown(tHeader, query.lang)
-    case 'order-processed':
-      return OrderProcessed(tHeader, options)
-    case 'next-action':
-      return NextAction(tHeader)
-    case 'returned':
-      return Returned(tHeader)
-    case 'live-tracking':
-      if (tHeader.actionBox.info && tHeader.courier && tHeader.courier.trackingurl)
-        return LiveTracking(tHeader, query, options.animateTruck || false)
-      else
-        break
+    const { type } = tHeader.actionBox
+    let result = null
+
+    if (type === 'pickup-location' && tHeader.actionBox.data) {
+      result = PickupLocation(tHeader, query.lang, emit)
     }
 
-    return [ // fallback
+    if (type === 'vote-courier') {
+      result = [
+        Fallback(tHeader, options),
+        VoteCourier(tHeader, options, emit)
+      ]
+    }
+
+    if (type === 'prediction') {
+      if (tHeader.actionBox.data &&
+        (tHeader.actionBox.data.dayOfWeek || tHeader.actionBox.data.deliveryLocation)) {
+        result = [
+          Prediction(tHeader),
+          DeliveryAddress(tHeader, query.lang)
+        ]
+      }
+    }
+
+    if (type === 'pickup-location-unknown') {
+      result = PickupLocationUnknown(tHeader, query.lang)
+    }
+
+    if (type === 'next-action') {
+      result = NextAction(tHeader)
+    }
+
+    if (type === 'returned') {
+      result = Returned(tHeader)
+    }
+
+    if (type === 'live-tracking') {
+      if (tHeader.actionBox.info && tHeader.courier && tHeader.courier.trackingurl) {
+        result = LiveTracking(tHeader, query, options.animateTruck || false)
+      }
+    }
+
+    result = result || [ // fallback
       Fallback(tHeader, options),
-      DeliveryAddress(tHeader, query.lang),
+      DeliveryAddress(tHeader, query.lang)
     ]
 
-
+    return result
   }
 }
 
