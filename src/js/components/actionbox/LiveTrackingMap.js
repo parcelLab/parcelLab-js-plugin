61599647830996ed572572729d6deb6d60f1c80b
Andrej Fritz <fritz.andrej@me.com>
2019-11-26 15:12:55 +0100

    fixed standardjs errs

--- src/js/components/actionbox/LiveTrackingMap.js
+++ src/js/components/actionbox/LiveTrackingMap.js
@@ -4,19 +4,15 @@ const GOOGLE_API_KEY = require('../../../../settings').google_api_key
 //   key: GOOGLE_API_KEY,
 // })
 
-const generateLinkSrc = coordinates =>
-  `https://www.google.com/maps/@${coordinates.long},${coordinates.lat},5z`
+// const generateLinkSrc = coordinates =>
+//   `https://www.google.com/maps/@${coordinates.long},${coordinates.lat},5z`
 
 const generateMapSrc = (coordinates, zoom) =>
   `https://www.google.com/maps/embed/v1/view?key=${GOOGLE_API_KEY}&center=${coordinates.long},${coordinates.lat}&zoom=5&maptype=roadmap`
 
-const generateTruckIconSrc = userId => `http://cdn.parcellab.com/img/mail/_/truckonmap/${userId}.png`
+// const generateTruckIconSrc = userId => `http://cdn.parcellab.com/img/mail/_/truckonmap/${userId}.png`
 
-////////
-// map//
-////////
-
-//DO RIGHT MAP --> Display truck!!
+// DO RIGHT MAP --> Display truck!!
 const Map = (id, actionBox) => {
   const elem = html`
     <div id="pl-pickup-location-map" data-tid="${id}">
@@ -25,15 +21,13 @@ const Map = (id, actionBox) => {
   `
 
   elem.isSameNode = function (target) { // dont rerender map if it is still the same tid
-    return id === target.dataset['tid']
+    return id === target.dataset.tid
   }
 
   return elem
 }
 
-//////////////
-// time box //
-//////////////
+/* -------------------------------- time box -------------------------------- */
 
 const checkTimeFormat = function (i) {
   if (i < 10) {
@@ -51,9 +45,7 @@ const generatePrettyTime = timeString => {
 }
 
 const DeliveryBox = (startTime, endTime, timeCaption, actionBox) => {
-
-
-  /* 
+  /*
   * TODO Caption for "Ankunftszeit ca... xx - xx"
   * TODO Ikea style scss (pl-box-body-userName)
   * Blue: HEX COLOR: #0051BA;
@@ -69,11 +61,10 @@ const DeliveryBox = (startTime, endTime, timeCaption, actionBox) => {
         <div class="pl-time-data">
           ${generatePrettyTime(startTime)} ${endTime ? ' - ' + generatePrettyTime(endTime) : ''}
         </div>
-        ${ timeCaption ? html`
+        ${timeCaption ? html`
         <small class="pl-time-caption">${timeCaption}</small>` : ''}
     `
 }
-
 
 const LiveTrackingMap = ({ id, actionBox }, query) => {
   if (!actionBox.coordinates) return null
@@ -98,7 +89,7 @@ const LiveTrackingMap = ({ id, actionBox }, query) => {
         ${Map(id, actionBox)}
       </div>
       <div class="pl-box-${query.userId}">
-        ${ deliveryBox}
+        ${deliveryBox}
       </div>
     </div>
     </div>
