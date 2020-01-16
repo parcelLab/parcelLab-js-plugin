83a5a12bdbbdd1e5310d5d21102907c2a69d0eda
Andrej Fritz <fritz.andrej@me.com>
2020-01-16 19:30:12 +0100

    ie fix

--- src/js/components/actionbox/LiveTrackingMap.js
+++ src/js/components/actionbox/LiveTrackingMap.js
@@ -2,6 +2,11 @@ const html = require('nanohtml')
 const { translate } = require('../../lib/translator')
 const Icon = require('../Icon')
 
+function parseIsoDatetime (dtstr) {
+  var dt = dtstr.split(/[: T-]/).map(parseFloat)
+  return new Date(dt[0], dt[1] - 1, dt[2], dt[3] || 0, dt[4] || 0, dt[5] || 0, 0)
+}
+
 function isIE () {
   var ua = window.navigator.userAgent
   // MSIE used to detect old browsers and Trident used to newer ones
@@ -10,8 +15,10 @@ function isIE () {
 
 const generateTime = timeStamp => {
   if (isIE()) {
+    // also for IE?
+    timeStamp = timeStamp ? parseIsoDatetime(timeStamp) : ''
     const date = timeStamp ? new Date(timeStamp) : new Date()
-    return `${date.getHours()}:${('0' + date.getMinutes()).slice(-2)}`
+    return `${('0' + date.getHours()).slice(-2)}:${('0' + date.getMinutes()).slice(-2)}`
   } else {
     // this is bug fix for safari since it cant parse yyyy-MM-dd dates
     timeStamp = timeStamp ? timeStamp.replace(/-/g, '/') : ''
