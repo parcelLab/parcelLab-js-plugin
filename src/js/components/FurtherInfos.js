7813092b631eacdc82ec7597b7afd09103d785d0
Andrej Fritz <fritz.andrej@me.com>
2020-02-25 11:00:52 +0100

    hide furtherinfos link

--- src/js/components/FurtherInfos.js
+++ src/js/components/FurtherInfos.js
@@ -4,7 +4,7 @@ const { translate } = require('../../js/lib/translator.js')
 const FurtherInfos = ({ courier, tracking_number }, langName) => {
   const sub = (tracking_number && tracking_number !== 'unknown') ? html`<div style="font-size:.9em;opacity:.6;">${ translate('delivery', langName) } ${ tracking_number }</div>` : null
 
-  if (courier && courier.trackingurl)
+  if (courier && courier.trackingurl && !courier.hide_trackingurl)
     return html`<a id="pl-courier-fwd-link" href="${courier.trackingurl}" target="_blank" class="pl-button pl-is-fullwidth">
       ${ courier.trackingurl_label }
       ${ sub }
