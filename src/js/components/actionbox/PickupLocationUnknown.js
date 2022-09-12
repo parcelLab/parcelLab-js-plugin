c692b002199b0fdacea541cb293e707adc7d0391
Lucas Cazeto <lucas.cazeto@parcellab.com>
2022-09-12 10:57:11 +0200

    TT-174/fix-no-action-box-for-unknow-pickup-location (#98)
    * chore: use fallback actionBox if no courier url
    
    * minor

--- src/js/components/actionbox/PickupLocationUnknown.js
+++ src/js/components/actionbox/PickupLocationUnknown.js
@@ -2,8 +2,6 @@ const html = require('nanohtml')
 const { translate } = require('../../../js/lib/translator.js')
 
 const PickupLocationUnknown = ({ actionBox, courier, last_delivery_status }, lang) => {
-  if (!courier || !courier.trackingurl) return null
-
   const status = last_delivery_status ? last_delivery_status.status : null
   return html`
     <div class="pl-box pl-action-box pl-box-pseudo-map">
@@ -28,23 +26,6 @@ const PickupLocationUnknown = ({ actionBox, courier, last_delivery_status }, lan
       </div>
     </div>
   `
-
-  // return html`
-  //   <div class="pl-box pl-action-box pl-box-pseudo-map">
-  //     <div class="pl-box-body">
-  //       <div id="pl-pseudo-map">
-  //         <a href="${courier.trackingurl}" title="${actionBox.label}" target="_blank">          
-  //           <div class="pl-pseudo-map-text">
-  //             <span>
-  //               ${ status }
-  //               ${ actionBox.label }
-  //             </span>
-  //           </div>
-  //         </a>
-  //       </div>
-  //     </div>
-  //   </div>
-  // `
 }
 
 module.exports = PickupLocationUnknown
